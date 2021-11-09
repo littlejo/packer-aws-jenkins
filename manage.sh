28a0592af8376832cdfaecb5d37fcf9743f08036
Joseph Ligier <littel.jo@gmail.com>
2021-11-09 17:29:54 +0000

    fix

--- manage.sh
+++ manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#packer build .
+packer build .
 ami=$(cat packer-manifest.json | jq -r .builds[0].artifact_id | awk -F: '{print $2}')
 echo "ami = \"$ami\"" > terraform/terraform.tfvars
